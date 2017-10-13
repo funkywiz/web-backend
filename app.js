4a6d585599da31394be35296edf368b6437edd15
Santhosh Mohan <santhoshm309@gmail.com>
2017-10-13 22:27:32 +0530

    CORS

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ console.log('DB connected');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Passport init
 app.use(passport.initialize());
